eafd94f2b5cc331136eadf99b0d8125c69e43fa3
Rio Ricardo S <rioricardos@gmail.com>
2024-11-16 18:12:29 +0700

    Changes for windows

--- commonlib/multicore/hardwareinfo.h
+++ commonlib/multicore/hardwareinfo.h
@@ -1,11 +1,18 @@
 #ifndef HARDWAREINFO_H
 #define HARDWAREINFO_H
 
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT
+#endif
+
 class HardwareInfo {
 public:
     // Returns the number of hardware cores available on the system
-    static unsigned int getHardwareCores();
+    static EXPORT unsigned int getHardwareCores();
 };
+
 
 #endif // HARDWAREINFO_H
 
