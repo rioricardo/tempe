73fa2da55958b322c392f27dff293408a848dc22
Rio Ricardo S <rioricardos@gmail.com>
2024-12-19 21:48:37 +0700

    change group with offset latest

--- kafka_test/src/example_consumer.cxx
+++ kafka_test/src/example_consumer.cxx
@@ -15,7 +15,7 @@ void startConsumer(const std::string& broker, const std::string& topic, const st
 int main() {
     std::string broker = "goodboy:9092";  // Kafka broker address
     std::string topic = "test_topic";  // Kafka topic to consume messages from
-    std::string groupid = "test_group";  // Kafka topic to consume messages from
+    std::string groupid = "test_group1";  // Kafka topic to consume messages from
     std::string offset = "latest";  // Kafka topic to consume messages from
 
     // Start consumer threads
